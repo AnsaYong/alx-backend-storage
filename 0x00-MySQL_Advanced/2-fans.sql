d04cdb49af9d3eafccfa35b49ce2ef1b8e81a7ca
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-25 16:19:15 +0200

    updated 2-fans.sql by changing column nb_fans to fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,8 @@
--- ranks country origins of bands, ordered by the number of (unique) fans
+-- ranks country origins of bands, ordered by the number of (non-unique) fans
 -- column names are origin, nb_fans
 -- origin is the country of origin of the band
 -- nb_fans is the number of unique fans that the bands have in that country
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY SUM(nb_fans) DESC;
+ORDER BY SUM(fans) DESC;
