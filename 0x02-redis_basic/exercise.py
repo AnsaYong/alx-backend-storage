43ae2dad6f0f9ff9f5cd67a9bac31fb0d1dfab41
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-01 15:12:36 +0200

    updated exercise.py by adding methods get, get_str, and get_int)

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ store an instance of a Redis client.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -56,3 +56,63 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+                                                    str, bytes, int, float]:
+        """
+        Gets the value of a key in the cache.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to retrieve from the cache.
+
+        fn : Callable
+            The function to apply to the value retrieved from the cache.
+
+        Returns
+        -------
+        value : str / bytes / int / float
+            The value of the key in the cache.
+        """
+
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Gets the value of a key in the cache as a string.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to retrieve from the cache.
+
+        Returns
+        -------
+        value : str
+            The value of the key in the cache as a string.
+        """
+
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Gets the value of a key in the cache as an integer.
+
+        Parameters
+        ----------
+        key : str
+            The key of the data to retrieve from the cache.
+
+        Returns
+        -------
+        value : int
+            The value of the key in the cache as an integer.
+        """
+
+        return self.get(key, fn=lambda x: int(x))
